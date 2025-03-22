3c29edb92464694a3ba42248449bac0d9d404459
HyunWoo Jeong <jrwedo@naver.com>
2025-03-23 00:08:33 +0900

    Feat: Component에 Unmount 생명 주기 추가로, 라우팅 시 기존 컴포넌트에서 등록한 글로벌 이벤트 제거하도록 구현

--- src/core/Component.js
+++ src/core/Component.js
@@ -6,6 +6,7 @@ export default class Component {
     constructor(props = {}) {
         this.props = props;
         this.$container = document.createElement("div"); // 페이지 컨테이너 생성
+        this._globalEvents = [];  // unmount 시 해제하기 위해 글로벌 이벤트 추적
         this.setup();
         this.setEvent();
         this.render();
@@ -28,6 +29,14 @@ export default class Component {
 
     mounted() {}
 
+    unmount() {
+        // 글로벌 이벤트 제거
+        this._globalEvents.forEach(({ eventType, callback }) => {
+            window.removeEventListener(eventType, callback);
+        });
+        this._globalEvents = [];
+    }
+
     setEvent() {}
 
     setState(newState) {
@@ -44,6 +53,7 @@ export default class Component {
 
     addGlobalEvent(eventType, callback) {
         window.addEventListener(eventType, callback);
+        this._globalEvents.push({ eventType, callback });
     }
 
     loadCSS(href) {
